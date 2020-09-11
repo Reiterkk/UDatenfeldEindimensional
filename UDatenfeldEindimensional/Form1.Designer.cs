8ceadf2c92d79e05cb67dce32e41be746c6f38d0
Kevin Reiter <reiter.kk@gmail.com>
2020-09-11 18:07:03 +0200

    Liste wird mit Zufallszahlen gefuellt.

--- UDatenfeldEindimensional/Form1.Designer.cs
+++ UDatenfeldEindimensional/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CmdMinima = new System.Windows.Forms.Button();
+            this.LstZahlen = new System.Windows.Forms.ListBox();
+            this.LblMinima = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdMinima
+            // 
+            this.CmdMinima.Location = new System.Drawing.Point(183, 89);
+            this.CmdMinima.Name = "CmdMinima";
+            this.CmdMinima.Size = new System.Drawing.Size(75, 23);
+            this.CmdMinima.TabIndex = 0;
+            this.CmdMinima.Text = "Minima";
+            this.CmdMinima.UseVisualStyleBackColor = true;
+            // 
+            // LstZahlen
+            // 
+            this.LstZahlen.Enabled = false;
+            this.LstZahlen.FormattingEnabled = true;
+            this.LstZahlen.ItemHeight = 15;
+            this.LstZahlen.Location = new System.Drawing.Point(25, 89);
+            this.LstZahlen.Name = "LstZahlen";
+            this.LstZahlen.Size = new System.Drawing.Size(120, 229);
+            this.LstZahlen.TabIndex = 1;
+            // 
+            // LblMinima
+            // 
+            this.LblMinima.AutoSize = true;
+            this.LblMinima.Location = new System.Drawing.Point(183, 124);
+            this.LblMinima.Name = "LblMinima";
+            this.LblMinima.Size = new System.Drawing.Size(63, 15);
+            this.LblMinima.TabIndex = 2;
+            this.LblMinima.Text = "Minimum:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblMinima);
+            this.Controls.Add(this.LstZahlen);
+            this.Controls.Add(this.CmdMinima);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CmdMinima;
+        private System.Windows.Forms.ListBox LstZahlen;
+        private System.Windows.Forms.Label LblMinima;
     }
 }
 
--- UDatenfeldEindimensional/Form1.cs
+++ UDatenfeldEindimensional/Form1.cs
@@ -12,10 +12,20 @@ namespace UDatenfeldEindimensional
 {
     public partial class Form1 : Form
     {
+        private readonly Random r = new Random();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] zahlen = new int[10];
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                zahlen[i] = r.Next(20, 31);
+                LstZahlen.Items.Add(zahlen[i]);
+            }
+        }
     }
 }
